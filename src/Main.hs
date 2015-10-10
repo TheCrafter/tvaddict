b7e6606e1ca9a32ddffbc5e2158ccd881ec8a7b8
TheCrafter <dimitrisvlh@gmail.com>
2015-10-10 04:34:23 +0300

    Added main loop and menu

--- src/Main.hs
+++ src/Main.hs
@@ -4,37 +4,84 @@ import Database as Db
 import Data.Aeson as Ae
 import System.Exit
 
-mySeries :: [Db.Series]
-mySeries = [ (Series "Gotham" (Episode 2 3))
-           , (Series "Person of Interest" (Episode 4 22))
-           , (Series "Supernatural" (Episode 1 11))
-           ]
+data MenuItem = MenuItem { msg   :: String
+                         , cmd   :: IO ()
+                         }
 
-searchTitle = "Flash"
+data Menu = Menu { items  :: [MenuItem]
+                 , header :: String
+                 }
 
-main = do
-   putStrLn "Creating new .tvaddict file..."
-   Db.createDbFile mySeries
+instance Show MenuItem where
+  show = msg
 
-   putStrLn "Adding Flash..."
-   Db.readDbFile >>= Db.insertSeries [Series "Flash" (Episode 1 2)]
+instance Show Menu where
+  show (Menu i h) = h ++ "\n" ++ (showItems 0 i)
 
-   find
+    where
+      showItems index strList =
+        case strList of
+          []     -> ""
+          (x:xs) -> (show index) ++ ". " ++ (show x) ++ "\n" ++ showItems (index + 1) xs
 
-   putStrLn "Updating Flash..."
-   Db.readDbFile >>= (Db.updateSeriesByTitle searchTitle $ Series "Flash" $ Episode 0 0)
+runMenuCmd :: Int -> IO ()
+runMenuCmd i = cmd $ (items myMenu) !! i
 
-   find
+myMenuItems =
+  [ (MenuItem "View all Series" printSeries)
+  , (MenuItem "Add series" addSeries)
+  , (MenuItem "Delete series" (putStrLn "TODO"))
+  , (MenuItem "Exit" exitSuccess)
+  ]
 
-   putStrLn "Deleteing Flash..."
-   Db.readDbFile >>= Db.deleteSeriesByTitle "Flash"
+myMenu = Menu myMenuItems "Choose one of the following:"
 
-   find
+printSeries :: IO ()
+printSeries = do
+  series <- Db.readDbFile
+  case series of
+    Nothing -> putStrLn "Couldn't read database file"
+    Just s  -> putStrLn $ showSeries s
 
   where
-    find = do
-      putStrLn $ "Trying to find " ++ searchTitle ++ "..."
-      result <- (Db.readDbFile >>= Db.findSeriesByTitle searchTitle)
-      case result of
-        Nothing -> putStrLn $ "Couldn't find " ++ searchTitle
-        Just x  -> putStrLn $ show x
+    showSeries s =
+      case s of
+        []     -> ""
+        (x:xs) -> showSerie x ++ "\n" ++ showSeries xs
+
+    showSerie x =
+      (title x) ++ ": Season " ++ (show $ season $curEpisode x) ++ ", Episode " ++ (show $ epNum $ curEpisode x)
+
+addSeries :: IO ()
+addSeries = do
+  putStrLn "Write the name of the series you want to add"
+  name <- getLine
+
+  putStrLn "Write the number of the season you're at"
+  season' <- getLine
+  let season = read season' :: Int
+
+  putStrLn "Write the number of the episode you're at"
+  episode' <- getLine
+  let episode = read episode' :: Int
+
+  let toAdd = Series name $ Episode season episode
+  series <- Db.readDbFile
+
+  -- Create dbfile if it doesnt exist
+  case series of
+    Nothing -> Db.createDbFile [toAdd]
+    Just _  -> Db.insertSeries [toAdd] series
+
+main = do
+  putStrLn $ show myMenu
+
+  answer <- getLine
+  case answer of
+    "0" -> runMenuCmd 0
+    "1" -> runMenuCmd 1
+    "2" -> runMenuCmd 2
+    "3" -> runMenuCmd 3
+    _   -> putStrLn "Invalid choice"
+
+  main
